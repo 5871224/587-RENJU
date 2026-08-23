ffb270e08b392afe20b745ea442a85f3fb736b04
5871224 <110716199+5871224@users.noreply.github.com>
2026-08-23 18:13:35 +0800

    Remove disabled legacy maintenance endpoint

--- bb/SQL.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Legacy database maintenance endpoint disabled on the public website.
-http_response_code(404);
-header('Content-Type: text/plain; charset=UTF-8');
-header('Cache-Control: no-store');
-echo 'Not Found';
-exit;
