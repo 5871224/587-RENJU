9cf16354bbcbac4d22fab2d876a7ed20d04c8d77
5871224 <110716199+5871224@users.noreply.github.com>
2026-09-14 17:55:17 +0800

    統一歷年比賽國際賽標示樣式

--- riftw/toura.php
+++ riftw/toura.php
@@ -69,17 +69,17 @@ ORDER BY `開始` DESC, `結束` DESC");
 
     $DATA = "";
     foreach ($statement as $row) {
-        $URL = "<a href='tourb.php?TOUR=" . $row['賽號'] . "'>" . $row['賽標'] . "</a>";
-
         if ($row['人數'] == 0) {
             $row['人數'] = "-";
         }
         if ($row['局數'] == 0) {
             $row['局數'] = "-";
         }
-        if (strpos($row['賽標'], "世界") !== false || strpos($row['賽標'], "亞洲") !== false) {
-            $row['賽標'] = "<font color='#ff00ff'>" . $row['賽標'] . "</font>";
-        }
+
+        $isInternational = strpos($row['賽標'], "世界") !== false || strpos($row['賽標'], "亞洲") !== false;
+        $linkClass = $isInternational ? " class='international-link'" : "";
+        $URL = "<a" . $linkClass . " href='tourb.php?TOUR=" . $row['賽號'] . "'>" . $row['賽標'] . "</a>";
+
         $DATA = $DATA . "<TR><TD>" . $row['開始'] . "</TD><TD>" . $URL . "</TD><TD>" . $row['人數'] . "</TD><TD>" . $row['局數'] . "</TD></TR>";
     }
     $DATA = $DATA . "</TABLE><BR/>";
